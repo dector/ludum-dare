3dd58678fc31d051d9fb8cd71c48691d5b540cd0
Denys P <dector@decdb.(none)>
2012-08-27 04:14:27 +0300

    [+] *LD24 entry*. Applet added

---
+++ ld24/src/ua/org/dector/ludumdare/ld24/Applet.java
@@ -0,0 +1,12 @@
+package ua.org.dector.ludumdare.ld24;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplet;
+
+/**
+ * @author dector
+ */
+public class Applet extends LwjglApplet {
+    public Applet() {
+        super(new MyGame(), false);
+    }
+}
